2672e4c03069d60cea1f352d1e52f4b71052ca67
priyanshup1 <72408237+priyanshup1@users.noreply.github.com>
2021-05-24 17:23:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,11 @@
-web: gunicorn main:mask-d-app --log-file=-
+mkdir -p ~/.streamlit/
+echo "\
+[gereral]\n\
+email=\"your-email@domain.com\"n\
+" > ~/.streamlit/creadentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS = false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
